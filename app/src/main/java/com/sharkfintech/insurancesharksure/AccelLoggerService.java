d9511e02dca817dc232912add79dbc8ec48b2d04
orcas <7555ic@gmail.com>
2016-07-30 11:25:14 +0800

    Scheduler

--- app/src/main/java/com/sharkfintech/insurancesharksure/AccelLoggerService.java
+++ app/src/main/java/com/sharkfintech/insurancesharksure/AccelLoggerService.java
@@ -1,13 +1,21 @@
 package com.sharkfintech.insurancesharksure;
 
 import android.app.Service;
+import android.app.job.JobInfo;
+import android.app.job.JobParameters;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.AsyncTask;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
 
 import java.util.ArrayList;
 
@@ -31,6 +39,13 @@ public class AccelLoggerService extends Service implements SensorEventListener {
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         sensorManager.registerListener(this, sensor,
                 SensorManager.SENSOR_DELAY_NORMAL);
+
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        JobInfo.Builder builder = new JobInfo.Builder( 1,
+                new ComponentName( getPackageName(),
+                        UploaderJobSchedule.class.getName() ) );
+        builder.setPeriodic(60000);
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_ANY);
 
         return START_STICKY;
     }
@@ -63,6 +78,35 @@ public class AccelLoggerService extends Service implements SensorEventListener {
             timeValues.add((System.currentTimeMillis() - time) / 1000f + "");
             accelValues.add(event.values[0] + "");
             return null;
+        }
+
+    }
+    private class UploaderJobSchedule extends JobService {
+        private Handler jobHandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                String[] timeArray = timeValues.toArray(new String[] {});
+                String[] accelArray = accelValues.toArray(new String[] {});
+                String csv = "";
+                for(int i = 0; i < timeArray.length; i++){
+                    csv += timeArray + ",";
+                    csv += accelArray + ",";
+                }
+                DataUploader uploader = new DataUploader("jerkymotion", "accel", csv);
+                jobFinished((JobParameters) msg.obj, false);
+                return false;
+            }
+        });
+        @Override
+        public boolean onStartJob(JobParameters params) {
+            jobHandler.sendMessage(Message.obtain(jobHandler, 1, params));
+            return true;
+        }
+
+        @Override
+        public boolean onStopJob(JobParameters params) {
+            jobHandler.removeMessages(1);
+            return false;
         }
 
     }
